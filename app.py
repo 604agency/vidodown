5aaa0488f442f0993a138d9058d4cc413236e8e9
604agency <info@604ajans.com>
2026-03-05 23:57:11 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def download_job(job_id, url, mode):
         }
     else:
         ydl_opts = {**base_opts,
-            "format": "best[ext=mp4]/best",
+            "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio/best",
         }
 
     try:
